f4bb364fd879cb0ee486a6a95ac838a4e189a082
helpbringer1v <132924895+helpbringer1v@users.noreply.github.com>
2023-05-08 16:15:32 +0100

    Create file.java

---
+++ file.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("test.txt");
+        
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        } finally {
+            System.out.println("End of program.");
+        }
+    }
+}
